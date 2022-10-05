8628f0834411de7258e75fe90e969746d5e6f261
fof002 <80104002+fof002@users.noreply.github.com>
2022-10-05 20:48:59 +0200

    yup

--- js/products.js
+++ js/products.js
@@ -1,26 +1,43 @@
 import { jacketArray } from "./constants/productList.js";
 
 const jacketContainer = document.querySelector(".jacket-specific-wrapper");
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const queryId = params.get("id");
 
-jacketArray.forEach( product => {
-    jacketContainer.innerHTML += `<img class="jacket-specific-image" src="../images/raincoat.png"alt="Picture of White Jacket"/>
-    <div class="jacket-specific-content">
-      <h1>${product.name}</h1>
-      <p>Description</p>
-      <p><b>Kr. ${product.price}</b></p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-        consectetur natus rerum eum ea illum optio excepturi omnis maxime
-        quidem?
-      </p>
-      <div class="size-container">
-        <div>Choose size</div>
-        <i class="fa-solid fa-angle-down"></i>
-      </div>
-      <div class="quantity-container">
-        <div class="checkout-button" id="${product.id}" class="checkout-button">Add to cart</div>
-      </div>
-    </div>` 
+function fetchJacket () {
+
+    try {
+
+    for (let i = 0; i < jacketArray.length; i++)
+
+    if (!i === queryId) {
+        continue;
+    }
+    else {
+        jacketContainer.innerHTML = `<img class="jacket-specific-image" src="../images/raincoat.png"alt="Picture of White Jacket"/>
+        <div class="jacket-specific-content">
+        <h1>${jacketArray[i].name}</h1>
+        <p>Description</p>
+        <p><b>Kr. ${jacketArray[i].price}</b></p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            consectetur natus rerum eum ea illum optio excepturi omnis maxime
+            quidem?
+        </p>
+        <div class="size-container">
+            <div>Choose size</div>
+            <i class="fa-solid fa-angle-down"></i>
+        </div>
+        <div class="quantity-container">
+            <div class="checkout-button" id="${jacketArray[i].id}" class="checkout-button">Add to cart</div>
+        </div>
+        </div>`; 
+    }
+    }
+    catch (error) {
+        jacketContainer.innerHTML = "Unable to load jacket";
+    }
 }
-);
 
+fetchJacket();
--- js/store.js
+++ js/store.js
@@ -1,23 +1,27 @@
-import { jacketArray } from "./constants/productList";
+import { jacketArray } from "./constants/productList.js";
 
 const jacketContainerStore = document.querySelector(".jackets-outer-grid-store");
-
 function fetchJackets () {
-    for (i=0; i<jacketArray.length; i++) {
-        let jacket = jacketArray[i];
-        jacketContainerStore.innerHTML = `<a href="../jacket-specific.html?id=${jacket.id}">
-        <div class="jackets-inner-grid-store">
-            <img src="../Images/Raincoat.png" alt="Photo of jacket" class="jacket-photo">
-            <div class="jacket-info">
-                <h2>${jacket.name}</h2>
-                <p>
-                    Men
-                </p>
-                <h3>
-                    kr. ${jacket.price}
-                </h3>
-            </div>
-        </div>`
+     try {
+        for (let i=0; i<jacketArray.length; i++) {
+            let jacket = jacketArray[i];
+            jacketContainerStore.innerHTML = `<a href="../jacket-specific.html?id=${jacket.id}">
+            <div class="jackets-inner-grid-store">
+                <img src="../Images/Raincoat.png" alt="Photo of jacket" class="jacket-photo">
+                <div class="jacket-info">
+                    <h2>${jacket.name}</h2>
+                    <p>
+                        Men
+                    </p>
+                    <h3>
+                        kr. ${jacket.price}
+                    </h3>
+                </div>
+            </div>`;
+        }
+     }
+    catch {
+        jacketContainerStore.innerHTML ="unable to load jackets!"
     }
 }
 fetchJackets();
