3eafc6ba31812f7911e80795fa3a2ed827f9d24a
fof002 <80104002+fof002@users.noreply.github.com>
2022-10-07 13:40:01 +0200

    b

--- js/form.js
+++ js/form.js
@@ -4,6 +4,8 @@ const email = document.querySelector("#email");
 const submitButton = document.querySelector(".submitButton");
 const emailError = document.querySelector(".emailError");
 const form = document.querySelector("#form");
+const firstName = document.querySelector("#firstname");
+const firstNameError = document.querySelector(".firstNameError");
 
 form.addEventListener("submit", validateForm);
 
@@ -16,6 +18,22 @@ function validateForm (event) {
     }
     else {
         emailError.style.display = "block";
+    }
+    if (checkLength(firstName,0)) {
+        firstNameError.style.display = "none";
+    }
+    else {
+        firstNameError.style.display = "block";
+    }
+  }
+
+  function checkLength (input, len) {
+    let lengthOfInput = input.value.trim().length;
+    if (lengthOfInput < len) {        
+        return true;
+    }
+    else {
+        return false;
     }
   }
 
